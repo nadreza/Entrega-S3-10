7fde225f6461545889db5b350d663cc6ff0695c2
unknown <machado.andreza@hotmail.com>
2022-04-13 13:43:05 -0300

    adicionando arquivos

---
+++ index.js
@@ -0,0 +1,10 @@
+function positions(firstPlace, secondPlace, lastPlace){
+   return firstPlace
+   
+
+}
+
+let posicoesDoPodio         = [firstPlace, secondPlace, lastPlace]
+let nomeDosCompetidores     = [Rafael, Manoel , Daniel]
+
+console.log(nomeDosCompetidores.length);
